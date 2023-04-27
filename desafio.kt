1f8bc4fecfc94b87fd800561638ed48714a6c4db
wallaxmello <34318410+wallaxmello@users.noreply.github.com>
2023-04-27 08:57:20 -0300

    Refactor Code

--- desafio.kt
+++ desafio.kt
@@ -1,6 +1,6 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
+eenum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 data class Usuario(var nome: String, val matricula: Int)
 
@@ -19,14 +19,18 @@ data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<Cont
 fun main() {
     //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    
     val usuarioUm = Usuario("user1", 123)
     val usuarioDois = Usuario("user2", 456)
     val usuarioTres = Usuario("user3", 789)
+    
     var conteudosEducacionais = mutableListOf<ConteudoEducacional>()
     conteudosEducacionais.add(ConteudoEducacional("Kotlin Introducao", duracao = 5))
     conteudosEducacionais.add(ConteudoEducacional("Kotlin Variaveis"))
     conteudosEducacionais.add(ConteudoEducacional("Kotlin Funcoes", duracao = 15))
+    
     val formacao = Formacao("Kotlin", Nivel.BASICO, conteudosEducacionais)
+    
     formacao.matricular(usuarioUm, usuarioDois, usuarioTres)
     println("Formação: " + formacao.nome + ", Nivel: " + formacao.nivel)
     formacao.conteudos.forEach{println("Conteudo: " + it.nome + ", Duracao: " + it.duracao + " min")}
