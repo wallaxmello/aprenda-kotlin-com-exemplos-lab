76657783e62fd2d037a3b7dd22d47179be729f59
wallaxmello <34318410+wallaxmello@users.noreply.github.com>
2023-04-27 07:52:46 -0300

    Start Code

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,34 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+data class Usuario(var nome: String, val matricula: Int)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 10)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val nivel: Nivel, var conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+    fun matricular(vararg usuario: Usuario) {
+        //TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+        inscritos.addAll(usuario)
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    //TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
+    //TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+    val usuarioUm = Usuario("user1", 123)
+    val usuarioDois = Usuario("user2", 456)
+    val usuarioTres = Usuario("user3", 789)
+    var conteudosEducacionais = mutableListOf<ConteudoEducacional>()
+    conteudosEducacionais.add(ConteudoEducacional("Kotlin Introducao", duracao = 5))
+    conteudosEducacionais.add(ConteudoEducacional("Kotlin Variaveis"))
+    conteudosEducacionais.add(ConteudoEducacional("Kotlin Funcoes", duracao = 15))
+    val formacao = Formacao("Kotlin", Nivel.BASICO, conteudosEducacionais)
+    formacao.matricular(usuarioUm, usuarioDois, usuarioTres)
+    println("Formação: " + formacao.nome + ", Nivel: " + formacao.nivel)
+    formacao.conteudos.forEach{println("Conteudo: " + it.nome + ", Duracao: " + it.duracao + " min")}
+    formacao.inscritos.forEach{println("Usuario: " + it.nome + ", Matricula: " + it.matricula)}
 }
